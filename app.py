c0f7ca3b0499a432a8fc2d99cb61254ed1a70538
Alana Bellucci <fastalana@gmail.com>
2020-04-12 14:22:54 -0600

    Simplifying the Hello, World! POC. Adding in a python requirement.

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
 from flask import Flask
 
-def create_app(test_config=None):
-    app = Flask(__name__)
+app = Flask(__name__)
 
 
-    @app.route('/')
-    def hello_world():
-        return "Hello, World!"
-
-    return app
-
-app = create_app()
+@app.route('/')
+def hello_world():
+    return "Hello, World!"
 
 if __name__ == '__main__':
     app.run()

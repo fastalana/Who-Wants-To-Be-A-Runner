b82d8ad6b8f4bc57217ce5371db2e4d48db341ea
Alana Bellucci <fastalana@gmail.com>
2020-04-17 09:23:14 -0600

    Adding additional tests to check for roles on endpoints.

--- test_app.py
+++ test_app.py
@@ -9,8 +9,8 @@ from models import setup_db, Athlete, Stat
 
 
 # USER GENERATED FOR TESTING PURPOSES ONLY
-# HAS PERMISSIONS: "get:requests", "post:requests", "patch:requests", "delete:requests", 
-MEMBER_TOKEN = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlhlbkxVTVBwNkFQR3FFNGVKeDVOaiJ9.eyJpc3MiOiJodHRwczovL2Rldi1mYXY1ZHA0ZC5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWU5OGI4YzIwZmI2YzYwYzgzYTQxZGJmIiwiYXVkIjoicnVubmVycyIsImlhdCI6MTU4NzA2NzI2NywiZXhwIjoxNTg3MTUzNjY3LCJhenAiOiJVQ0hrank2RlJDb2lLU1R5bWpJQUtrTVFHckVvUjl1YyIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOlsiZGVsZXRlOnJlcXVlc3RzIiwiZ2V0OnJlcXVlc3RzIiwicGF0Y2g6cmVxdWVzdHMiLCJwb3N0OnJlcXVlc3RzIl19.eY5WNecE6rNQNp9otUFUZ1EncLc2YQpfHWSp330HDmWsAGa7vGXKhKarxNLPaq121ov07Rr6IAtD-VswcW1jQX65wuKqn51TDr-0LZlj0r-xg40Bgq8kV3FN9grKKM7NnmLRVTic8ALjp1q_VBWWRwPINIeBqWyou218cjk6qmXF_cszd8TO7lboiMdaoBq_BHg0Jrf_321udIBfwSQ-YW6XXV7ib1HFituj5cb8n3aFDlvz_5bbnoCpinyks_Um-nyOmF2NyDaeYicUFpGcUzcNbhbzf9QgjLI2OfaoLteuoBWzdkoAT0uIs8ldqrefAblybUW3bfOhJ2XA9Vvr1A'
+# HAS ALL PERMISSIONs: "get:all_athletes", "get:all_stats", "post:athlete", "post:stat", "patch:stat", "delete:stat"
+MEMBER_TOKEN = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlhlbkxVTVBwNkFQR3FFNGVKeDVOaiJ9.eyJpc3MiOiJodHRwczovL2Rldi1mYXY1ZHA0ZC5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWU5OGI4YzIwZmI2YzYwYzgzYTQxZGJmIiwiYXVkIjoicnVubmVycyIsImlhdCI6MTU4NzEzNDU2NSwiZXhwIjoxNTg3MjIwOTY1LCJhenAiOiJVQ0hrank2RlJDb2lLU1R5bWpJQUtrTVFHckVvUjl1YyIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOlsiZGVsZXRlOnN0YXQiLCJnZXQ6YWxsX2F0aGxldGVzIiwiZ2V0OmFsbF9zdGF0cyIsInBhdGNoOnN0YXQiLCJwb3N0OmF0aGxldGUiLCJwb3N0OnN0YXQiXX0.MVbc4lJqskIuupBApkDG8cGcA_pWGdt5eyrJ2K0eQp5I4x5T-F1o3GomBEMFumlPSXBEE2I1E1Ks-SHIGTJ9eC8BqF8wFgLC4LlCLTO3tRjIxt5rKJeWhrFACX5Ss8lcxNrr7sCuuMmg9OD9LAah0bTOM9Y_yF5e6qLYf4ZQ5dR2ORsu3YWvwcbS4PV1aRUg58-6W6JscH3sU61gvF53gv0rTyqSa1WJfX1lmOIolw_lNwVFAWMgC4aFAWM-JXHA5pBcT-FXwpChNWm-DXWY1SgispwHTt-f7afg3VEbHeHupVAzV0Jaryj8Y0vPkt-97aGDiwwQUa_vUt6psMOJHA'
 
 class RunnerTestCase(unittest.TestCase):
     
@@ -30,20 +30,48 @@ class RunnerTestCase(unittest.TestCase):
         pass
 
     # GET ENDPOINTS
-    def test_get_athletes(self):
+    def test_get_athletes_member(self):
+        self.headers.update({'Authorization': 'Bearer ' + MEMBER_TOKEN})
+        response = self.client.get('/athletes', headers=self.headers)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_athletes_public(self):
         response = self.client.get('/athletes')
         data = json.loads(response.data)
 
         self.assertEqual(response.status_code, 200)
+        # self.assertEqual(response.status_code, 401)
 
-    def test_get_stats(self):
+    def test_get_stats_member(self):
+        self.headers.update({'Authorization': 'Bearer ' + MEMBER_TOKEN})
+        response = self.client.get('/stats', headers=self.headers)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_stats_public(self):
         response = self.client.get('/stats')
         data = json.loads(response.data)
 
         self.assertEqual(response.status_code, 200)
+        # self.assertEqual(response.status_code, 401)
     
     # PUT ENDPOINTS
-    def test_add_athlete(self):
+    def test_add_athlete_member(self):
+        new_athlete = {
+            "first_name": "Amelia",
+            "last_name": "Boone"
+        }
+
+        self.headers.update({'Authorization': 'Bearer ' + MEMBER_TOKEN})
+        response = self.client.post('/athletes', json=new_athlete, headers=self.headers)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_add_athlete_public(self):
         new_athlete = {
             "first_name": "Amelia",
             "last_name": "Boone"
@@ -53,8 +81,27 @@ class RunnerTestCase(unittest.TestCase):
         data = json.loads(response.data)
 
         self.assertEqual(response.status_code, 200)
+        # self.assertEqual(response.status_code, 401)
 
-    def test_add_stat(self):
+    def test_add_stat_member(self):
+        new_stat = {
+            "athlete_id":"1", 
+            "avg_miles_per_week":"2", 
+            "avg_vertical_per_week":"3", 
+            "longest_run":"4", 
+            "longest_run_2_weeks":"5", 
+            "race_distance":"6", 
+            "race_veritcal":"7", 
+            "race_date":"2020-08-09"
+        }
+
+        self.headers.update({'Authorization': 'Bearer ' + MEMBER_TOKEN})
+        response = self.client.post('/stats', json=new_stat, headers=self.headers)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_add_stat_public(self):
         new_stat = {
             "athlete_id":"1", 
             "avg_miles_per_week":"2", 
@@ -70,9 +117,21 @@ class RunnerTestCase(unittest.TestCase):
         data = json.loads(response.data)
 
         self.assertEqual(response.status_code, 200)
+        # self.assertEqual(response.status_code, 401)
 
     # PATCH ENDPOINT
-    def test_update_stat(self):
+    def test_update_stat_member(self):
+        updated_stat = {
+            "athlete_id":"1"
+        }
+
+        self.headers.update({'Authorization': 'Bearer ' + MEMBER_TOKEN})
+        response = self.client.patch('/stats/1', json=updated_stat, headers=self.headers)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_stat_public(self):
         updated_stat = {
             "athlete_id":"1"
         }
@@ -81,14 +140,22 @@ class RunnerTestCase(unittest.TestCase):
         data = json.loads(response.data)
 
         self.assertEqual(response.status_code, 200)
+        # self.assertEqual(response.status_code, 401)
 
     # DELETE ENDPOINT
-    def test_delete_stat(self):
+    def test_delete_stat_member(self):
+        self.headers.update({'Authorization': 'Bearer ' + MEMBER_TOKEN})
+        response = self.client.patch('/stats/1', headers=self.headers)
+        data = json.loads(response.data)
 
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_stat_public(self):
         response = self.client.patch('/stats/1')
         data = json.loads(response.data)
 
         self.assertEqual(response.status_code, 200)
+        # self.assertEqual(response.status_code, 401)
 
 # Make the tests conviently executable
 if __name__ == "__main__":

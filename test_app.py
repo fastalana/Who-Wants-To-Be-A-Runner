e75ddec5282f2e4ab878003a7dd457b5bef882df
Alana Bellucci <fastalana@gmail.com>
2020-04-17 11:09:03 -0600

    Updating formatting based on PycodeStyle Errors.

--- test_app.py
+++ test_app.py
@@ -9,14 +9,18 @@ from models import setup_db, Athlete, Stat
 
 
 # MEMBER USER GENERATED FOR TESTING PURPOSES ONLY
-# HAS ALL PERMISSIONS: "get:all_athletes", "get:all_stats", "post:athlete", "post:stat", "patch:stat", "delete:stat"
+# HAS ALL PERMISSIONS:
+# "get:all_athletes", "get:all_stats",
+# "post:athlete", "post:stat", "patch:stat", "delete:stat"
 MEMBER_TOKEN = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlhlbkxVTVBwNkFQR3FFNGVKeDVOaiJ9.eyJpc3MiOiJodHRwczovL2Rldi1mYXY1ZHA0ZC5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWU5OGI4YzIwZmI2YzYwYzgzYTQxZGJmIiwiYXVkIjoicnVubmVycyIsImlhdCI6MTU4NzEzNDU2NSwiZXhwIjoxNTg3MjIwOTY1LCJhenAiOiJVQ0hrank2RlJDb2lLU1R5bWpJQUtrTVFHckVvUjl1YyIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOlsiZGVsZXRlOnN0YXQiLCJnZXQ6YWxsX2F0aGxldGVzIiwiZ2V0OmFsbF9zdGF0cyIsInBhdGNoOnN0YXQiLCJwb3N0OmF0aGxldGUiLCJwb3N0OnN0YXQiXX0.MVbc4lJqskIuupBApkDG8cGcA_pWGdt5eyrJ2K0eQp5I4x5T-F1o3GomBEMFumlPSXBEE2I1E1Ks-SHIGTJ9eC8BqF8wFgLC4LlCLTO3tRjIxt5rKJeWhrFACX5Ss8lcxNrr7sCuuMmg9OD9LAah0bTOM9Y_yF5e6qLYf4ZQ5dR2ORsu3YWvwcbS4PV1aRUg58-6W6JscH3sU61gvF53gv0rTyqSa1WJfX1lmOIolw_lNwVFAWMgC4aFAWM-JXHA5pBcT-FXwpChNWm-DXWY1SgispwHTt-f7afg3VEbHeHupVAzV0Jaryj8Y0vPkt-97aGDiwwQUa_vUt6psMOJHA'
 
 # PUBLIC USER GENERATED FOR TESTING PURPOSED ONLY
 # HAS NO PERMISSIONS
 PUBLIC_TOKEN = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlhlbkxVTVBwNkFQR3FFNGVKeDVOaiJ9.eyJpc3MiOiJodHRwczovL2Rldi1mYXY1ZHA0ZC5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWU5OWNkYjg5NjA3MTEwYzllY2U3ZDNjIiwiYXVkIjoicnVubmVycyIsImlhdCI6MTU4NzEzODA0MCwiZXhwIjoxNTg3MjI0NDQwLCJhenAiOiJVQ0hrank2RlJDb2lLU1R5bWpJQUtrTVFHckVvUjl1YyIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOltdfQ.LnhMw1XzfJynYvN_S9s40kjBGmphQ0kD1knWsObap6p79CWK1q2LXb7ZBehX3yph2vLSh5fO53oImgLmtetziT5e-nEMBsxk2EHxX4yAjmN2rXJ3wsp0eAWQNEBuk3lOE2LjDBEE3YsfToHlStS0ktD0Ecei-h8-Ct-yrSsUicfqw1uyW2ETnthCPhI_yfa9TO5rb5bk_S5g_RPDf56P7FzhByeb5EpsmgkXyIpzROykO7VNptAThM3RnwXmQ1PxKJ4nA7Sqan1w8VjFLANrtgZQXoNX65wDnG9QbMDzBYqFdm5fnUzVO7fbP_fhsF9I_YXtMNbjrP8vRrTlROsPQg'
+
+
 class RunnerTestCase(unittest.TestCase):
-    
+
     def setUp(self):
         app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://alanabellucci@localhost:5432/runners_test'
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -24,7 +28,7 @@ class RunnerTestCase(unittest.TestCase):
         self.client = app.test_client()
         self.headers_member = {'Content-Type': 'application/json', 'Authorization': MEMBER_TOKEN}
         self.headers_public = {'Content-Type': 'application/json', 'Authorization': PUBLIC_TOKEN}
-        
+
         db.drop_all()
         db.create_all()
 
@@ -58,7 +62,7 @@ class RunnerTestCase(unittest.TestCase):
 
         self.assertEqual(response.status_code, 200)
         # self.assertEqual(response.status_code, 401)
-    
+
     # PUT ENDPOINTS
     def test_add_athlete_member(self):
         new_athlete = {
@@ -85,14 +89,14 @@ class RunnerTestCase(unittest.TestCase):
 
     def test_add_stat_member(self):
         new_stat = {
-            "athlete_id":"1", 
-            "avg_miles_per_week":"2", 
-            "avg_vertical_per_week":"3", 
-            "longest_run":"4", 
-            "longest_run_2_weeks":"5", 
-            "race_distance":"6", 
-            "race_veritcal":"7", 
-            "race_date":"2020-08-09"
+            "athlete_id": "1",
+            "avg_miles_per_week": "2",
+            "avg_vertical_per_week": "3",
+            "longest_run": "4",
+            "longest_run_2_weeks": "5",
+            "race_distance": "6",
+            "race_veritcal": "7",
+            "race_date": "2020-08-09"
         }
 
         response = self.client.post('/stats', json=new_stat, headers=self.headers_member)
@@ -102,14 +106,14 @@ class RunnerTestCase(unittest.TestCase):
 
     def test_add_stat_public(self):
         new_stat = {
-            "athlete_id":"1", 
-            "avg_miles_per_week":"2", 
-            "avg_vertical_per_week":"3", 
-            "longest_run":"4", 
-            "longest_run_2_weeks":"5", 
-            "race_distance":"6", 
-            "race_veritcal":"7", 
-            "race_date":"2020-08-09"
+            "athlete_id": "1",
+            "avg_miles_per_week": "2",
+            "avg_vertical_per_week": "3",
+            "longest_run": "4",
+            "longest_run_2_weeks": "5",
+            "race_distance": "6",
+            "race_veritcal": "7",
+            "race_date": "2020-08-09"
         }
 
         response = self.client.post('/stats', json=new_stat, headers=self.headers_public)
@@ -121,7 +125,7 @@ class RunnerTestCase(unittest.TestCase):
     # PATCH ENDPOINT
     def test_update_stat_member(self):
         updated_stat = {
-            "athlete_id":"1"
+            "athlete_id": "1"
         }
 
         response = self.client.patch('/stats/1', json=updated_stat, headers=self.headers_member)
@@ -131,7 +135,7 @@ class RunnerTestCase(unittest.TestCase):
 
     def test_update_stat_public(self):
         updated_stat = {
-            "athlete_id":"1"
+            "athlete_id": "1"
         }
 
         response = self.client.patch('/stats/1', json=updated_stat, headers=self.headers_public)
@@ -153,6 +157,7 @@ class RunnerTestCase(unittest.TestCase):
 
         self.assertEqual(response.status_code, 200)
         # self.assertEqual(response.status_code, 401)
+
 
 # Make the tests conviently executable
 if __name__ == "__main__":
